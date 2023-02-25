822ba5854b1543b50efaa373c30e7f50e63ac287
Cesario87 <ces.albred@gmail.com>
2023-02-25 16:03:58 +0100

    added/details

--- src/components/Main/Details/Details.jsx
+++ src/components/Main/Details/Details.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Details = () => {
-  return <div>Details</div>;
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const data = JSON.parse(decodeURIComponent(searchParams.get("data")));
+
+  return (
+    <div>
+      <h2>{data.name}</h2>
+      <img src={data.image} alt={data.name} />
+      <p>ID: {data.id}</p>
+      <p>Type 1: {data.typeOne}</p>
+      <p>Type 2: {data.typeTwo ? data.typeTwo : "none"}</p>
+    </div>
+  );
 };
 
 export default Details;
