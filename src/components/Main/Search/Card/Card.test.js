cd1ddd7af2e8b7af1209fdee0280acb94fa3875e
Cesario87 <ces.albred@gmail.com>
2023-03-02 13:59:48 +0100

    fixed/tests

--- src/components/Main/Search/Card/Card.test.js
+++ src/components/Main/Search/Card/Card.test.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Card from './Card';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Cards from './Card';
 
 test('Card renders', () => {
-  const pokemonData = { name:"ditto", id:"132", height:"3", weight:"40", types:[{type: {name:"normal"}}], sprites:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" };
-  render(<Card pokemon={pokemonData} />);
+  const pokemonData = { id: "132", name: "ditto", sprites: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png', types: [{ type: { name: "normal" } }, { type: { name: "none" } }] };
+  render(<Router>
+    <Cards pokemon={pokemonData} />
+  </Router>
+  );
   const cardElement = screen.getByTestId('cardRenders');
   expect(cardElement).toBeVisible();
 });
 
 test('Title exists', () => {
-  const pokemonData = { name:"ditto", id:"132", height:"3", weight:"40", types:[{type: {name:"normal"}}], sprites: { front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" }};
-  render(<Card pokemon={pokemonData} />);
+  const pokemonData = { id: "132", name: "ditto", sprites: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png', types: [{ type: { name: "normal" } }, { type: { name: "none" } }] };
+  render(<Router>
+    <Cards pokemon={pokemonData} />
+  </Router>
+  );
   const titleElement = screen.getByTestId('Card__title');
   expect(titleElement).toBeInTheDocument();
   const imgElement = screen.getByTestId('Card__img');
@@ -19,15 +26,21 @@ test('Title exists', () => {
 });
 
 test('Card renders the name from props in the title', () => {
-  const pokemonData = { name:"ditto", id:"132", height:"3", weight:"40", types:[{type: {name:"normal"}}], sprites: { front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" }};
-  render(<Card pokemon={pokemonData} />);
+  const pokemonData = { id: "132", name: "Ditto", sprites: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png', types: [{ type: { name: "normal" } }, { type: { name: "none" } }] };
+  render(<Router>
+    <Cards pokemon={pokemonData} />
+  </Router>
+  );
   const titleElement = screen.getByTestId('Card__title');
   expect(titleElement).toHaveTextContent(pokemonData.name);
 });
 
 test('Card renders the sprite image from props', () => {
-  const pokemonData = { name:"ditto", id:"132", height:"3", weight:"40", types:[{type: {name:"normal"}}], sprites: { front_default:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" }};
-  render(<Card pokemon={pokemonData} />);
+  const pokemonData = { id: "132", name: "ditto", sprites: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png', types: [{ type: { name: "normal" } }, { type: { name: "none" } }] };
+  render(<Router>
+    <Cards pokemon={pokemonData} />
+  </Router>
+  );
   const imgElement = screen.getByTestId('Card__img');
-  expect(imgElement).toHaveAttribute('src', pokemonData.sprites.front_default);
+  expect(imgElement).toHaveAttribute('src', pokemonData.sprites);
 });
